81d3052c86ab49fd44733d5938d8ba43247db557
Eric Satoshi <satoshi.eric@gmail.com>
2020-11-16 16:57:35 -0300

    Atualização

--- Projeto/scripts/game.js
+++ Projeto/scripts/game.js
@@ -9,109 +9,127 @@ class Jogo {
     constructor(altura, largura) {
         this.altura = altura
         this.largura = largura
-        this.quadrados = []
-        this.indexTetrominoAtual = 0
-        this.pilhaTetrominos = []
-        this.rotacaoAtual = this.tetrominos[Math.floor(Math.random() * 7)]
-        this.rotacaoAtual = 0
-        this.tetromino_I = [
-            [0, largura, 2*largura, 3*largura],
+
+        this.definindoTetrominos()
+        this.criarTabuleiro()
+
+        this.quadrados = document.querySelectorAll(".tabuleiro > div")
+    }
+
+    definindoTetrominos() {
+        const tetromino_I = [
+            [0, this.largura, 2*this.largura, 3*this.largura],
             [0, 1, 2, 3],
-            [0, largura, 2*largura, 3*largura],
+            [0, this.largura, 2*this.largura, 3*this.largura],
             [0, 1, 2, 3]
         ]
-        this.tetromino_O = [
-            [0, 1, largura, largura+1],
-            [0, 1, largura, largura+1],
-            [0, 1, largura, largura+1],
-            [0, 1, largura, largura+1]
+        const tetromino_O = [
+            [0, 1, this.largura, this.largura+1],
+            [0, 1, this.largura, this.largura+1],
+            [0, 1, this.largura, this.largura+1],
+            [0, 1, this.largura, this.largura+1]
         ]
-        this.tetromino_L1 = [
-            [0, largura, 2*largura, 2*largura+1],
-            [0, 1, 2, largura],
-            [0, 1, largura+1, 2*largura+1],
-            [2, largura, largura+1, largura+2]
+        const tetromino_L1 = [
+            [0, this.largura, 2*this.largura, 2*this.largura+1],
+            [0, 1, 2, this.largura],
+            [0, 1, this.largura+1, 2*this.largura+1],
+            [2, this.largura, this.largura+1, this.largura+2]
         ]
-        this.tetromino_L2 = [
-            [0, 0+largura, 1+largura, 2+largura],
-            [0, 1, 0+largura, 0+2*largura],
-            [0, 1, 2, 2+largura],
-            [1, 1+largura, 1+2*largura, 2*largura]
+        const tetromino_L2 = [
+            [0, 0+this.largura, 1+this.largura, 2+this.largura],
+            [0, 1, 0+this.largura, 0+2*this.largura],
+            [0, 1, 2, 2+this.largura],
+            [1, 1+this.largura, 1+2*this.largura, 2*this.largura]
         ]
-
-        this.tetromino_T = [
-            [0, 1, 2, largura+1],
-            [0, largura, largura+1, 2*largura],
-            [1, largura, largura+1, largura+2],
-            [1, largura, largura+1, 2*largura+1]
+    
+        const tetromino_T = [
+            [0, 1, 2, this.largura+1],
+            [0, this.largura, this.largura+1, 2*this.largura],
+            [1, this.largura, this.largura+1, this.largura+2],
+            [1, this.largura, this.largura+1, 2*this.largura+1]
         ]
-
-        this.tetromino_U = [
-            [0, 2, largura, largura+1, largura+2],
-            [0, 1, largura, 2*largura, 2*largura+1],
-            [0, 1, 2, largura, largura+2],
-            [0, 1, largura+1, 2*largura, 2*largura+1]
+    
+        const tetromino_U = [
+            [0, 2, this.largura, this.largura+1, this.largura+2],
+            [0, 1, this.largura, 2*this.largura, 2*this.largura+1],
+            [0, 1, 2, this.largura, this.largura+2],
+            [0, 1, this.largura+1, 2*this.largura, 2*this.largura+1]
         ]
-
-        this.specialTetromino = [0]
-
+    
+        const specialTetromino = [0]
+    
         this.tetrominos = [
-            this.tetromino_I,
-            this.tetromino_O,
-            this.tetromino_L1,
-            this.tetromino_L2,
-            this.tetromino_T,
-            this.tetromino_U,
-            this.specialTetromino
+            tetromino_I,
+            tetromino_O,
+            tetromino_L1,
+            tetromino_L2,
+            tetromino_T,
+            tetromino_U,
+            specialTetromino
         ]
     }
 
+    /**
+     * Cria todos os quadrados do tabuleiro
+     * Cada quadrado deverá ter 1 de 3 estados disponíveis:
+     * - estatico:  quadrados sem tetrominos
+     * - tetromino: quadrados definidos com tetrominos
+     * - parado: tetrominos que não podeem mais se mover pois estão em cima dee outros ou da base do jogo
+     */
     criarTabuleiro() {
-        this.quadrados = []
-        for (let i=0; i<this.altura*this.largura; i++) {
-            this.quadrados.push(0)
-        }
-    }
-
-    desenharTabuleiro() {
         const tabuleiro = document.querySelector(".tabuleiro")
-        tabuleiro.innerHTML = ""
-        for (let i in this.quadrados) {
-            tabuleiro.innerHTML += "<div class='quadrado'></div>"
+        for (let i=0; i<this.altura*this.largura; i++) {
+            tabuleiro.appendChild(document.createElement("div"))
         }
+
+        const quadrados = document.querySelectorAll(".tabuleiro > div")
+        quadrados.forEach((value) => {
+            value.classList.add("estatico")
+        })
     }
 
-    desenharTetromino(tetrominoType) {
-        const quadrados = document.querySelectorAll(".quadrado")
-        this.indexTetrominoAtual = Math.floor(Math.random() * 7)
-        this.pilhaTetrominos.push(this.tetrominos[this.index])
+    /** 
+     * Cria o próximo tetromino a cair
+     */
+    criarTetromino() {
+        const index = Math.floor(Math.random() * 7)
+        const tetrominoAtual = this.tetrominos[index]
 
-        for (let i in this.tetrominos[tetrominoType][this.indexTetrominoAtual]) {
-            quadrados[this.tetrominos[tetrominoType][this.indexTetrominoAtual][i]].style.backgroundColor = 'red'
-        }
+        tetrominoAtual[0].forEach((value) => {
+            this.quadrados[value].classList.remove("estatico")
+            this.quadrados[value].classList.add("tetromino")
+        })
     }
 
-    apagarTetromino(tetrominoType) {
-        const quadrados = document.querySelectorAll(".quadrado")
-        
-        for (let i in this.tetrominos[tetrominoType][this.indexTetrominoAtual]) {
-            quadrados[this.tetrominos[tetrominoType][this.indexTetrominoAtual][i]].style.backgroundColor = 'white'
-        }
+    /**
+     * Desenha o estado atual do jogo
+     */
+    desenhar() {
+        this.quadrados.forEach((quadrado) => {
+            if(quadrado.classList.contains("tetromino"))
+                quadrado.style.backgroundColor = "red"
+        })
     }
 
-    descer(tetrominoType) {
-        const quadrados = document.querySelectorAll(".quadrado")
-        
-        for (let i in this.tetrominos[tetrominoType][this.indexTetrominoAtual]) {
-            quadrados[this.tetrominos[tetrominoType][this.indexTetrominoAtual][i]]
+    apagar() {
+        this.quadrados.forEach((quadrado) => {
+            if(quadrado.classList.contains("tetromino"))
+                quadrado.classList.remove("tetromino")
+        })
+    }
+
+    descer() {
+        const quadrados = this.quadrados
+        for (let i in quadrados) {
+            if (quadrados[i].classList.contains("tetromino"))
+                quadrados[i+this.largura].classList.add("tetromino")
         }
+    }
+    
+    atualizar() {
+
     }
 }
 
+
 const jogo = new Jogo(20, 10)
-
-jogo.criarTabuleiro()
-jogo.desenharTabuleiro()
-jogo.desenharTetromino(1)
-jogo.apagarTetromino(1)
-
