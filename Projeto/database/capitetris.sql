214244f9e3a871f6344f9770be6a757c751f7040
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-01-02 13:40:00 -0300

    Alteração do .sql

--- Projeto/database/capitetris.sql
+++ Projeto/database/capitetris.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02-Jan-2021 às 16:58
+-- Tempo de geração: 02-Jan-2021 às 17:31
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.4.11
 
@@ -29,11 +29,28 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tbl_ranking` (
   `id_ranking` int(11) NOT NULL,
-  `fk_id_perfil` int(11) NOT NULL,
+  `fk_id_usuario` int(11) NOT NULL,
   `level` int(11) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
   `lines` int(11) DEFAULT NULL,
   `time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbl_usuario`
+--
+
+CREATE TABLE `tbl_usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` char(100) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `cpf` char(14) NOT NULL,
+  `telefone` char(9) NOT NULL,
+  `email` char(150) NOT NULL,
+  `username` char(100) NOT NULL,
+  `password` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -45,7 +62,13 @@ CREATE TABLE `tbl_ranking` (
 --
 ALTER TABLE `tbl_ranking`
   ADD PRIMARY KEY (`id_ranking`),
-  ADD KEY `fk_id_perfil` (`fk_id_perfil`);
+  ADD KEY `fk_id_usuario` (`fk_id_usuario`);
+
+--
+-- Índices para tabela `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -58,6 +81,12 @@ ALTER TABLE `tbl_ranking`
   MODIFY `id_ranking` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restrições para despejos de tabelas
 --
 
@@ -65,7 +94,7 @@ ALTER TABLE `tbl_ranking`
 -- Limitadores para a tabela `tbl_ranking`
 --
 ALTER TABLE `tbl_ranking`
-  ADD CONSTRAINT `tbl_ranking_ibfk_1` FOREIGN KEY (`fk_id_perfil`) REFERENCES `tbl_perfil` (`id_perfil`);
+  ADD CONSTRAINT `tbl_ranking_ibfk_1` FOREIGN KEY (`fk_id_usuario`) REFERENCES `tbl_usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
